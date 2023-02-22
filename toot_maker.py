28400034a20d7a58b2085cb542410c8849ed0f26
pikesley <sam.pikesley@gmail.com>
2023-02-22 21:15:32 +0000

    Make multiple sentences

--- toot_maker.py
+++ toot_maker.py
@@ -1,3 +1,4 @@
+import random
 from pathlib import Path
 
 import markovify
@@ -11,7 +12,20 @@ def make_toot():
     """Make a toot."""
     # we could serialise this, but it's *extremely* quick to generate, so ¯\_(ツ)_/¯
     text_model = markovify.Text(Path("tweets.txt").read_text(encoding="utf-8"))
-    return text_model.make_short_sentence(max_length)
+
+    sentences = []
+    character_count = 0
+
+    while character_count < max_length:
+        sentence = ""
+        # while not sentence:
+        sentence = text_model.make_short_sentence(max_length / 2)
+
+        character_count += len(sentence)
+        sentences.append(sentence)
+
+    random.shuffle(sentences)
+    return " ".join(sentences)
 
 
 if __name__ == "__main__":
